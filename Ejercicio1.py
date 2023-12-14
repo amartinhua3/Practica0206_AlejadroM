4bac1c097656605b05d9e583cf0214772555af11
amartinhua3 <amartinhua3@educacion.navarra.es>
2023-12-14 14:56:58 +0100

    Ejercicio1

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+file = open('tabla-n.txt', 'w')
+
+def numero(num):
+    #n = int(input('Introduzca un número entero entre 1 y 10: '))
+    for i in range(0, 11):
+        file.write(str(num * i)+ '\n')
+
+
+numero(7)
