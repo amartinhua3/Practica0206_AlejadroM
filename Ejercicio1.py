17ce55f36f0d149da5fad452f1a301e19f5ca0be
amartinhua3 <amartinhua3@educacion.navarra.es>
2024-06-08 14:35:12 +0200

    ejercicio1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,11 +1,11 @@
 
 
 def numero():
-    
+    '''Esta función recibe un numero introducido por el usuario y crea una tabla de '''
     n = int(input('Introduzca un número entero entre 1 y 10: '))
     file = open(f'tabla-{n}.txt', 'w')
 
     for i in range(0, 11):
-        file.write(str(n * i)+ '\n')
+        file.write(str(f'{n} x {i} =  {n * i}') + '\n')
 
 numero()
